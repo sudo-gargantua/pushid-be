f9df789e9357c40fda982b8b568adb1c915bf38d
Dimas Agung <kadekdimasagungwinu3@gmail.com>
2025-12-24 15:50:19 +0800

    feat: Add initial CORS configuration file.

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000', 'https://api-pushid.undiksha.cloud')),
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:3000,https://api-pushid.undiksha.cloud,https://pushid.undiksha.cloud')),
 
     'allowed_origins_patterns' => [],
 
